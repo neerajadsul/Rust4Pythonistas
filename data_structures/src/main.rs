fc34579a5088385102a27e68ae8b07d2c2b0c976
neerajadsul <neeraj.adsul@gmail.com>
2024-03-30 22:55:36 +0000

    Tuples and traits for tuple.

--- data_structures/src/main.rs
+++ data_structures/src/main.rs
@@ -1,9 +1,34 @@
 use std::collections::HashSet;
 
+#[derive(Debug)]
+struct Mat2x2(f32, f32, f32, f32);
+
+impl Mat2x2 {
+    fn comb(&self) -> [f32; 4]  {
+        [self.0, self.1, self.2, self.3]
+    }    
+
+    fn determinant(&self) -> f32 {
+        self.0 * self.3 - self.1 * self.2
+    }
+}
+
 fn main() {
     println!("Unpacking Tuple");
     unpacking();
     unpacking_arbitrary();
+
+    let tup = ("Tupple", 2.5, 37_i32);
+    let reversed = reverse_triplet(tup);
+    println!("{:?}", reversed);
+
+    let square_mat = Mat2x2(1.5, -2f32, -0.5, 4.2);
+    println!("{:?}", square_mat);
+
+    for item in square_mat.comb() {
+        println!("{}", item);
+    }
+    println!("{}", square_mat.determinant());
 }
 
 /*python
@@ -27,4 +52,10 @@ fn unpacking_arbitrary() {
     match record {
         [x, y @ ..] => println!("{:?} {:?}", x, y)
     };
+}
+
+
+fn reverse_triplet(t: (&str, f32, i32)) -> (i32, f32, &str) {
+    println!("{:?}", t);
+    (t.2, t.1, t.0)
 }
